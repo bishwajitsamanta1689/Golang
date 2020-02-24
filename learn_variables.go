8f848b48569f5d82b60eb30b82519b133c4fc872
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-24 19:47:18 +0530

    Adding Variables

---
+++ learn_variables.go
@@ -0,0 +1,12 @@
+package main 
+
+import "fmt"
+
+func main () {
+	var myInt = 16
+	var val, ok = "bishwajit", true
+	fmt.Println("myInt:", myInt)
+	fmt.Println("myInt times two:", myInt*2)
+	fmt.Println("val:", val)
+	fmt.Println("ok:", ok)
+}
