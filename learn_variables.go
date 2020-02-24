73f3ad57ad9260806a52686965e54a8c96c6e400
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-24 19:54:57 +0530

    Adding _ as variables

--- learn_variables.go
+++ learn_variables.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main () {
-	var myInt = 16
-	var val, ok = "bishwajit", true
+	// var myInt = 16
+	myInt := 16
+	// var val, ok = "bishwajit", true
+	var val, _ = "bishwajit", true
 	fmt.Println("myInt:", myInt)
 	fmt.Println("myInt times two:", myInt*2)
 	fmt.Println("val:", val)
-	fmt.Println("ok:", ok)
+	// fmt.Println("ok:", ok)
 }
