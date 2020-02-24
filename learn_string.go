81ad627fdf3c1c74f79727d56db7de6a73042afd
Bishwajeett Uday Samanta <bishwajit.samanta@smarsh.com>
2020-02-24 13:18:23 +0530

    String in Golang

---
+++ learn_string.go
@@ -0,0 +1,14 @@
+package main
+
+// Import Modules
+
+import "fmt"
+
+func main () {
+	fmt.Println("Simple String ")
+	fmt.Println(`
+	This is a Multi Line
+	String`)
+	fmt.Println("Some emojis!!")
+	fmt.Println("Unicode Character : \u2272")
+}
